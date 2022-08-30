8cbe07b710c7ab95f016dc45024e3c8ed607de16
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 00:17:31 +0530

    Update convert2pdf.py

--- convert2pdf.py
+++ convert2pdf.py
@@ -2,19 +2,23 @@ from PIL import Image
 import streamlit as st
 import os
 
-filename = st.file_uploader("Choose a file")
+filename = st.file_uploader("Choose a file" , type=None )
+
+# if uploaded_file is not None:
+#      dataframe = pd.read_csv(uploaded_file)
+#      st.write(dataframe)
+     
 image = Image.open(filename)
 st.image(image, caption='image file')
 
 if image.mode == "RGBA":
      image = image.convert("RGB")
 
-# output ='output.pdf'
 image = image.save("output.pdf") 
 
 with open("output.pdf", "rb") as file:
      btn = st.download_button(
-             label="Download image",
+             label="Download pdf file !",
              data=file,
              file_name="output.pdf",
              mime="image/pdf"
