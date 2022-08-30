3f765c818aab29469c6b1d9d89cc4ec7ee9f86ac
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-30 23:28:20 +0530

    Update convert2pdf.py

--- convert2pdf.py
+++ convert2pdf.py
@@ -3,11 +3,6 @@ import streamlit as st
 import os
 
 filename = st.file_uploader("Choose a file")
-if filename is not None:
-     # To read file as bytes:
-     bytes_data = filename.getvalue()
-     st.write(bytes_data)
-
 image = Image.open(filename)
 st.image(image, caption='Sunrise by the mountains')
 
@@ -15,12 +10,12 @@ if image.mode == "RGBA":
      image = image.convert("RGB")
 
 output ='output.pdf'
-# image.save(output) 
+image.save(output) 
 
-with open("output", "rb") as file:
+with open("image.png", "rb") as file:
      btn = st.download_button(
              label="Download image",
-             data=file,
+             data=filename,
              file_name="output",
              mime="image/png"
            )
