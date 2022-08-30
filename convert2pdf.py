6a400521e3dd234141d98c3a2115af54153fe88c
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-30 23:09:11 +0530

    Update convert2pdf.py

--- convert2pdf.py
+++ convert2pdf.py
@@ -3,6 +3,11 @@ import streamlit as st
 import os
 
 filename = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
+
 image = Image.open(filename)
 
 if image.mode == "RGBA":
