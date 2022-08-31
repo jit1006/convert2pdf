f8b413b9a74a5f224fd0859849d9c999925a8f37
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2022-08-31 19:53:46 +0530

    Update convert2pdf.py

--- convert2pdf.py
+++ convert2pdf.py
@@ -5,20 +5,21 @@ import os
 filename = st.file_uploader("Choose a file", accept_multiple_files=False , key = None ) 
 if filename is not None:
         image = Image.open(filename)
-    else:
+else:
+        
         image = Image.open("output.png")
           
 st.image(image, caption='UPLOADED FILE ! ')
 
 if image.mode == "RGBA":
-     image = image.convert("RGB")
+        image = image.convert("RGB")
 
 image = image.save("output.pdf") 
 
 with open("output.pdf", "rb") as file:
-     btn = st.download_button(
-             label="Download pdf file !",
-             data=file,
-             file_name="output.pdf",
-             mime="image/pdf"
-           )
+        btn = st.download_button(
+                label="Download pdf file !",
+                data=file,
+                file_name="output.pdf",
+                mime="image/pdf"
+                )
